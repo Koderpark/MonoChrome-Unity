a57e0186309afe2453716399fc12f2a2647b26b6
박성훈 / Koder <koder0205@gmail.com>
2020-06-24 18:29:24 +0900

    update
    오늘 점심 업데이트

--- MonoChrome/Assets/script/SetSetting.cs
+++ MonoChrome/Assets/script/SetSetting.cs
@@ -3,27 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Configuration;
+using System.Xml;
+using System.Diagnostics;
+using System.Runtime.Versioning;
+using UnityEngine.SceneManagement;
 
 public class SetSetting : MonoBehaviour
 {
-    public InputField id;
+    public InputField playername;
     public Scrollbar bgm;
     public Scrollbar sfx;
 
-    void Start()
+    private void Start()
     {
-        if (PlayerPrefs.HasKey("id"))
-        {
-            id.text = PlayerPrefs.GetString("id");
-            bgm.value = PlayerPrefs.GetFloat("bgm");
-            sfx.value = PlayerPrefs.GetFloat("sfx");
-        }
+        TextAsset txt = (TextAsset)Resources.Load("setting");
+        XmlDocument setxml = new XmlDocument();
+        setxml.LoadXml(txt.text);
+
+        playername.text = setxml.GetElementsByTagName("playerName")[0].InnerText;
+        bgm.value = float.Parse(setxml.GetElementsByTagName("bgm")[0].InnerText);
+        sfx.value = float.Parse(setxml.GetElementsByTagName("sfx")[0].InnerText);
+
+        UnityEngine.Debug.Log("test");
     }
     public void SettingExit()
     {
-        PlayerPrefs.SetString("id", id.text);
-        PlayerPrefs.SetFloat("bgm", bgm.value);
-        PlayerPrefs.SetFloat("sfx", sfx.value);
+        TextAsset txt = (TextAsset)Resources.Load("setting");
+        XmlDocument xml = new XmlDocument();
+        xml.LoadXml(txt.text);
+
+        xml.GetElementsByTagName("playerName")[0].InnerText = playername.text;
+        xml.GetElementsByTagName("bgm")[0].InnerText = bgm.value.ToString();
+        xml.GetElementsByTagName("sfx")[0].InnerText = sfx.value.ToString();
+
+        xml.Save("./Assets/Resources/setting.xml");
         SceneManager.LoadScene("MainScene");
     }
 }
---
+++ MonoChrome/Assets/script/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class Shop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("id"))
+        {
+            //id.text = PlayerPrefs.GetString("id");
+            //bgm.value = PlayerPrefs.GetFloat("bgm");
+            //sfx.value = PlayerPrefs.GetFloat("sfx");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
